44c4c15036364f77a1c59a1320a4d3d86e1b4648
Kaio Aime Garcia <kaiogarcia@employer.com.br>
2024-03-11 20:42:41 -0300

    ponto e virgula

--- ApiEstudo/Repository/UserRepository.cs
+++ ApiEstudo/Repository/UserRepository.cs
@@ -12,7 +12,7 @@
 
         public UserRepository(MysqlContext context)
         {
-            _context = context
+            _context = context;
         }
 
         public User ValidateCredentials(UserVO userVO)
