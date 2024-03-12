f2f3f9c6a07f51d1489402f3b7854ab6e4639f02
Kaio Aime Garcia <kaiogarcia@employer.com.br>
2024-03-12 20:11:57 -0300

    Implementando TokenService

--- ApiEstudo/Repository/UserRepository.cs
+++ ApiEstudo/Repository/UserRepository.cs
@@ -3,6 +3,7 @@
     using ApiEstudo.Data.VO;
     using ApiEstudo.Model;
     using ApiEstudo.Model.Context;
+    using System.Data;
     using System.Security.Cryptography;
     using System.Text;
 
@@ -20,6 +21,31 @@
             var pass = ComputeHash(userVO.Password, new SHA256CryptoServiceProvider());
 
             return _context.Users.FirstOrDefault(u => (u.UserName == userVO.UserName) && (u.Password == pass));
+        }
+
+        public User RefreshUserInfo(User user)
+        {
+            if (!_context.Users.Any(u => u.Id.Equals(user.Id))) return null;
+
+            var result = _context.Users.SingleOrDefault(p => p.Id.Equals(user.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(user);
+
+                    _context.SaveChanges();
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            return result;
         }
 
         private string ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
---
+++ ApiEstudo/Services/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiEstudo.Services
+{
+    using System.Security.Claims;
+
+    public interface ITokenService
+    {
+        string GenerateAcessToken(IEnumerable<Claim> claims);
+
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
